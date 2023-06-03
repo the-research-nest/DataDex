7f3996933ebe490984515e964a017f6e1b6f1e0a
aditya-xq <adityavivek1998@gmail.com>
2023-06-03 15:22:29 +0530

    using manifest v3

--- popup.ts
+++ popup.ts
@@ -1,1 +1,8 @@
 // TODO: Add popup logic
+
+document.addEventListener('DOMContentLoaded', function() {
+    const messageElement = document.getElementById('message');
+    if (messageElement) {
+      messageElement.textContent = 'Hello, World!';
+    }
+  });  
