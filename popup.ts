106acbef0167f10386f6d1364e590d2d9bde65a0
aditya-xq <adityavivek1998@gmail.com>
2023-06-03 15:23:44 +0530

    hello world

--- popup.ts
+++ popup.ts
@@ -1,8 +1,2 @@
 // TODO: Add popup logic
-
-document.addEventListener('DOMContentLoaded', function() {
-    const messageElement = document.getElementById('message');
-    if (messageElement) {
-      messageElement.textContent = 'Hello, World!';
-    }
-  });  
+ 
